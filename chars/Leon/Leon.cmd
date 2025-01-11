eab6717167599c527090878ae8d52e5631bc9895
JonTheCopycat <eljonnu0906@gmail.com>
2025-01-11 17:18:22 -0500

    Leon WD A replacement 2 and bugfix
    Leon:
    1. (-) Wavedash A has been replaced:
    	* Instead of armor, it is partially invul to aerial attacks and all projectiles during the roll.
    	* Startup drastically increased, reach drastically increased.
    	* Wavedash cancellable.
    2. (~) No longer able to fastfall during 214A

--- chars/Leon/Leon.cmd
+++ chars/Leon/Leon.cmd
@@ -632,7 +632,7 @@ type = VelSet
 triggerall = statetype = A && movetype != H
 triggerall = command = "down"
 triggerall = (vel y > 0 && vel y < 6) || stateno = 66 || stateno = 105
-triggerall = stateno != 900  && stateno != [21000, 21999]
+triggerall = stateno != 900  && stateno != [21000, 21999] && stateno != [22000, 22999]
 trigger1 = 1
 y = 6
 
