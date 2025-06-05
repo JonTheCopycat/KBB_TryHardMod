8077f1c0ad240ec154504ff465b55ab86ffac822
JonTheCopycat <eljonnu0906@gmail.com>
2025-06-05 19:36:16 -0400

    Character Balance Changes and Fixes:
    Aege:
    (-) Wavedash B: no longer possible to fastfall during this move.
    (-) Wavedash B: can no longer be cancelled into air skip, j.5a, j.5b, and j.5b. Now cancellable into j.2c
    (-) 5c: cost increased (200 -> 250)
    (-) 5c.c: cost increased (100 -> 250)
    
    Clutch:
    (+) Fixed bugged dash cancel state.
    
    Glitch:
    (-) j.2c: No longer invul to aerial attacks and throws. Now exclusively invul to projectiles.
    
    Saito:
    (+) Wavedash C: Reworked to be a forward moving stance. No hitbox, but can cancel into stance moves quickly.
    (+) New Move: Wavedash C.3C. Similar to divekick, but from this stance and slightly different.
    (+) New Move: Wavedash C.xC.C: Saito can now reenter this stance with a short attack after each of the stance-only attacks.
    (~) Velocities Adjusted for all Wavedash C follow ups.
    (-) Wavedash C.2C and C.5C: removed movement cancels (jump, wavedash, fastfall)
    (+) Wavedash C.6C: launch velocity increased (10 -> 12)

--- chars/Aege/Aege.cmd
+++ chars/Aege/Aege.cmd
@@ -611,7 +611,7 @@ type = VelSet
 triggerall = statetype = A
 triggerall = command = "down" && movetype != H
 triggerall = (vel y > 0 && vel y < 6) || stateno = 66 || stateno = 105
-triggerall = stateno != 900 && stateno != 24500
+triggerall = stateno != 900 && stateno != 390 && stateno != 24500
 trigger1 = 1
 y = 6
 
